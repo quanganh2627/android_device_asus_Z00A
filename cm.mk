7168db1f456731438efd29ac45be0c1ea4f2d0e7
quanganh2627 <quanganh2627@gmail.com>
2016-06-04 23:52:33 +0700

    Build with java 8
    Change-Id: I94b5a048aa02765bcbab20949d5f98a058859227

--- cm.mk
+++ cm.mk
@@ -16,6 +16,8 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 DEVICE_PACKAGE_OVERLAYS += device/asus/Z00A/overlay
 
 PRODUCT_RUNTIMES := runtime_libart_default
+BLOCK_BASED_OTA := false
+EXPERIMENTAL_USE_JAVA8 := false
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_Z00A
@@ -24,7 +26,7 @@ PRODUCT_MODEL := ASUS_Z00A
 PRODUCT_MANUFACTURER := asus
 PRODUCT_DEVICE := Z00A
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=WW_Z00A \
-    BUILD_FINGERPRINT=asus/WW_Z00A/Z00A:5.0/LRX21V/2.20.40.165_20160118_6541_user:user/release-keys \
-    PRIVATE_BUILD_DESC="asusmofd_fhd-user 5.0 LRX21V 2.20.40.165_20160118_6541_user release-keys"
+#~ PRODUCT_BUILD_PROP_OVERRIDES += \
+#~     PRODUCT_NAME=WW_Z00A \
+#~     BUILD_FINGERPRINT=asus/WW_Z00A/Z00A:5.0/LRX21V/2.20.40.165_20160118_6541_user:user/release-keys \
+#~     PRIVATE_BUILD_DESC="asusmofd_fhd-user 5.0 LRX21V 2.20.40.165_20160118_6541_user release-keys"
